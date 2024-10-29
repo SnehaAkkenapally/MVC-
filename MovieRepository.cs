734e856488d4b241761eb33e0d8d5a4fece44528
SnehaAkkenapally <sneha.akkenapally11@gmail.com>
2024-10-28 20:49:11 -0400

    Add files via upload

---
+++ MovieRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository
+{
+    public class MovieRepository : IMovieRepository
+    {
+        public IEnumerable<MovieCardModel> GetTopMovies()
+        {
+            return new List<MovieCardModel>
+            {
+                new MovieCardModel { Id = 1, Title = "Inception" },
+                new MovieCardModel { Id = 2, Title = "The Matrix" }
+            };
+        }
+    }
+}
